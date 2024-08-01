0c3edab88b66d389924b4b56fc583fb8077ac8a6
GaylordCC <gcarricaballero3@hotmail.com>
2024-08-01 16:54:16 -0500

    Test openai and chatopenai in main method

--- main_streaming_v2.py
+++ main_streaming_v2.py
@@ -3,7 +3,9 @@ from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 from openai import AsyncOpenAI
 
-from langchain_openai import OpenAI
+from langchain_openai import ChatOpenAI, OpenAI
+
+import time
 
 app = FastAPI()
 client = AsyncOpenAI()
@@ -19,11 +21,23 @@ app.add_middleware(
 
 @app.get("/main-streaming-endpoint")
 async def main():
-    response_text = ""
-    llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0, max_tokens=512)
+    start_time = time.time()  # Start timing
 
+    # OpenAI(model="gpt-3.5-turbo-instruct", temperature=0, max_tokens=512)
+    response_text = ""
+    llm = ChatOpenAI(model="gpt-4", temperature=0, max_tokens=512)
+    messages =[("Write me a song about sparkling water.")]
     async def event_stream():
-        for chunk in llm.stream("Write me a song about sparkling water."):
+        for chunk in llm.stream(messages):
             yield f"data: {chunk}\n\n"
             
-    return StreamingResponse(event_stream(), media_type="text/event-stream")
+    response = StreamingResponse(event_stream(), media_type="text/event-stream")
+
+    end_time = time.time()  # End timing
+
+    # Calculate the time taken
+    time_taken = end_time - start_time
+
+    print(f"Time taken to generate response: {time_taken} seconds")
+
+    return response
